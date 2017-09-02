b076084c3b6ddf3347824aa8e4a6112a08b1c626
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-02 22:45:26 +0530

    Setting up the parent App component

--- React Signup/src/App.js
+++ React Signup/src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 
 class App extends Component {
- 
+ constructor(props){
+   super(props);
+
+   this.state = {
+     mounted: false
+   };
+
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({ mounted: false });
+  }
+
+  componentDidMount(){
+    return (
+      this.setState({ mounted: true })
+    )
+  }
 
 
   render() {
+    let items;
+    if (this.state.mounted) {
+      items = (
+        <Modal onSubmit={this.handleSubmit} />
+      );
+    }
     return (
       <div className="App">
-        App
+        <ReactCSSTransitionGroup 
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+        >
+          {items}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
