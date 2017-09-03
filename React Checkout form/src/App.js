7de54da6361badb65b698dcda14e705d08ef6ee3
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-03 23:35:45 +0530

    Added the App and Container

--- React Checkout form/src/App.js
+++ React Checkout form/src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Overlay from './components/Overlay';
+import Container from './components/Container';
+import { CSSTransitionGroup } from 'react-transition-group' 
 
 class App extends Component {
- 
+  constructor(props){
+    super(props);
+    this.state = {
+      mounted: false,
+      people: 1,
+      price: 320.00,
+      tax: 20,
+      duration: 5,
+      discount: 5
+    };
+  }
 
 
   render() {
+    var overlay, container;
+
+    if(this.state.mounted){
+      overlay = (
+        <Overlay img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg"/>
+      );
+
+      container = (
+        <Container />
+      );
+    }
     return (
       <div className="App">
-        <Overlay img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg"/>
+        <CSSTransitionGroup transitionName="overlay" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {overlay}
+        </CSSTransitionGroup>
+
       </div>
     );
   }
---
+++ React Checkout form/src/components/Container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Container extends Component{
+    render() {
+        return(
+            <div className="Container">
+                I like being loyal
+            </div>
+        );
+    }
+}
+
+export default Container;
