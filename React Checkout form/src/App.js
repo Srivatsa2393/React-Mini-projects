c662e2a34487d6cd4fedd2c019c5a8101694e85e
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-03 23:49:44 +0530

    Added the imagepreview and checkout component

--- React Checkout form/src/App.js
+++ React Checkout form/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Overlay from './components/Overlay';
 import Container from './components/Container';
-import { CSSTransitionGroup } from 'react-transition-group' 
+import { CSSTransitionGroup } from 'react-transition-group';
+import ImagePreview from './components/ImagePreviewArea/ImagePreview';
+import Checkout from './components/CheckoutArea/Checkout';
 
 class App extends Component {
   constructor(props){
@@ -15,6 +17,13 @@ class App extends Component {
       duration: 5,
       discount: 5
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log('handled ajax submission here');
   }
 
 
@@ -27,7 +36,23 @@ class App extends Component {
       );
 
       container = (
-        <Container />
+        <Container>
+          <ImagePreview 
+            price={this.state.price} 
+            duration={this.state.duration}
+            people={this.state.people}
+            image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg"
+          />
+
+          <Checkout 
+            duration={this.state.duration}
+            discount={this.state.discount}
+            tax={this.state.tax}
+            price={this.state.price}
+            onSubmit={this.handleSubmit}
+          />
+
+        </Container>
       );
     }
     return (
@@ -35,6 +60,11 @@ class App extends Component {
         <CSSTransitionGroup transitionName="overlay" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
           {overlay}
         </CSSTransitionGroup>
+
+        <CSSTransitionGroup transitionName="container" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {container}
+        </CSSTransitionGroup>
+
 
       </div>
     );
---
+++ React Checkout form/src/components/ImagePreviewArea/ImagePreview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ImagePreview extends Component{
+    render() {
+        return(
+            <div className="ImagePreview">
+
+            </div>
+        );  
+    }
+}
+
+export default ImagePreview;
