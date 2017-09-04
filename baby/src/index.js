2a06e22eb95ccdacc98d84c4e78618bf01438bb2
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-04 22:29:27 +0530

    Using data as props in App component

--- baby/src/index.js
+++ baby/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import ('normalize.css');
+import data from './data/data';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 registerServiceWorker();
