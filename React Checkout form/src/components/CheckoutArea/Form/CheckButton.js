28b5f86a39d832d454a64d18ab46ecafd01cd17a
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-05 23:55:07 +0530

    refactoring

--- React Checkout form/src/components/CheckoutArea/Form/CheckButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CheckButton = (props) => {
-    return (
-        <div />
-    );
-}
-
-export default CheckButton;
--- React Checkout form/src/components/CheckoutArea/Form/ExpiryDate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ExpiryDate = (props) => {
-    return (
-        <div />
-    );
-}
-
-export default ExpiryDate;
