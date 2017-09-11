46150e2249b657a07bbe730517e6227873fc9ec5
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-11 23:22:43 +0530

    Helper method to determine the users location using freegeoip.net

--- React App icon/src/Icon.js
+++ React App icon/src/Icon.js
@@ -7,9 +7,36 @@ class Icon extends Component{
             this.state={
                 time: 1,
                 location: '',
-                temp: ''
+                temp: '',
+                icon: '',
+                weather_code: ''
             };
     }
+
+
+    //whenever the component loads we must fetch IP and render it to the component
+    //to fetch ip i have used freegeoip.net
+    componentDidMount(){
+        this.fetchIP();
+    }
+
+    //fetchIP to determine the IP
+    fetchIP() {
+        fetch('freegeoip.net/json/')
+            .then((response) => {
+                return response.json();
+            })//fetching the city by using open weather sample
+            .then((data) => {
+                const city = data.city;
+                this.fetchWeatherDate(city);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    
+
     render() {
         return(
             <div className="Icon" data-hour={this.state.time}>
