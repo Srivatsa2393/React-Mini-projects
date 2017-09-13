dcf41304131c232da03a7c325d9f8364329060ef
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-13 23:07:58 +0530

    added the settings component

--- Reactgram/src/App.js
+++ Reactgram/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Settings from './Settings';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      image: 'https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg'
+    };
+  }
   render() {
     return (
       <div className="App">
-        App component
+        <div className="ImageBG" style={{backgroundImage: 'url('+ this.state.image + ')'}}>
+        </div>
+        <Settings image={this.state.image} />
       </div>
     );
   }
