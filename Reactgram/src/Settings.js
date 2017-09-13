3a629cf655a24722328f5cfa773c30d8132f780b
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-13 23:25:16 +0530

    Added the state for Settings component

--- Reactgram/src/Settings.js
+++ Reactgram/src/Settings.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import Setting from './Setting';
 
 class Settings extends Component{
+constructor(props){
+    super(props);
+    this.state = {
+        contrast: '100',
+        hue: '0',
+        brightness: '100',
+        saturate: '100',
+        sepia: 0
+    };
+}
+
+handleChange(e){
+    e.preventDefault();
+}
+
     render() {
         return(
             <div className="Settings">
@@ -10,7 +25,11 @@ class Settings extends Component{
 
                         <div className="Title">Reactgram by Srivatsa</div>
 
-                        <Setting />
+                        <Setting name="contrast" value={this.state.contrast} onChange={this.handleChange}></Setting>
+                        <Setting name="hue" value={this.state.hue} onChange={this.handleChange}></Setting>
+                        <Setting name="brightness" value={this.state.brightness} onChange={this.handleChange}></Setting>
+                        <Setting name="saturate" value={this.state.saturate} onChange={this.handleChange}></Setting>
+                        <Setting name="sepia" value={this.state.sepia} onChange={this.handleChange}></Setting>
 
                     </div>
                 </div>
