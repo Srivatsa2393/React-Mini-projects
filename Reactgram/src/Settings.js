2956bb4dfc65b7bafab95d8f7ac7cda91d3a03f0
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-13 23:17:05 +0530

    Added the setting component

--- Reactgram/src/Settings.js
+++ Reactgram/src/Settings.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Setting from './Setting';
 
 class Settings extends Component{
     render() {
         return(
-            <div>
-                Settings
+            <div className="Settings">
+                <div className="MainWrapper">
+                    <div className="Sidebar">
+
+                        <div className="Title">Reactgram by Srivatsa</div>
+
+                        <Setting />
+
+                    </div>
+                </div>
             </div>
         );
     }
