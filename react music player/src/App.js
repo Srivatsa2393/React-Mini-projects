3a2fefd45886845feabc522e215583dd36b0a5bb
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-19 20:38:59 +0530

    Added the basic TrackList component which displays the list of tracks

--- react music player/src/App.js
+++ react music player/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TrackList from './TrackList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App component
+        <TrackList />
       </div>
     );
   }
---
+++ react music player/src/TrackList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrackList extends Component{
+    render(){
+        return(
+            <div>
+                Track list components
+            </div>
+        );
+    }
+}
+
+export default TrackList;
