1559ef86a883da181becd421ea422465a6b497d3
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-19 20:48:18 +0530

    Setting up the tracklist component

--- react music player/src/TrackList.js
+++ react music player/src/TrackList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import data from './tracks.json';
 
 class TrackList extends Component{
+    components(props){
+        super(props);
+        this.state = {
+            tracks: []
+        };
+        this.renderListItem = this.renderListItem.bind(this);
+    }
+    componentDidMount() {
+        //fetching all the tracks 
+        this.setState({ tracks: data.tracks });
+    }
+
+    renderListItem(track, i){
+         
+    }
+
     render(){
+        let tracks = this.state.tracks.map(this.renderListItem);
         return(
-            <div>
-                Track list components
-            </div>
+            <ul className="TrackList">
+
+            </ul>
         );
     }
 }
