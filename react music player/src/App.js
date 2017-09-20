87e1fdef9fef80f95c09a24d7e61e00aa7e74ac5
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-20 16:10:10 +0530

    setting up the component level state in the App component

--- react music player/src/App.js
+++ react music player/src/App.js
@@ -3,10 +3,23 @@ import TrackList from './TrackList';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      playing: false,
+      currentTrackIndex: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
   render() {
     return (
       <div className="App">
-        <TrackList />
+        <div 
+          className="Artwork"
+          style={{ backgroundImage:  'url(' + data.artwork + ')'}}
+        >
+          <Controls onClick={this.handleClick} playing={this.state.playing} />
+        </div>
       </div>
     );
   }
