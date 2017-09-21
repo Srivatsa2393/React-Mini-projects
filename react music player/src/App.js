fc93f97f606cc93fcbb0c4ce473e53cc258df97e
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-21 18:54:32 +0530

    Music player logic ready only styling left

--- react music player/src/App.js
+++ react music player/src/App.js
@@ -4,8 +4,8 @@ import data from './tracks.json';
 import Controls from './Controls';
 import './App.css';
 
-class App extends Component {
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       playing: false,
@@ -16,97 +16,72 @@ class App extends Component {
     this.pauseAudio = this.pauseAudio.bind(this);
     this.selectTrackNumber = this.selectTrackNumber.bind(this);
   }
-
-  selectTrackNumber(trackId) {
-    this.setState({ currentTrackIndex: trackId, playing: true }, this.playAudio);
-  }
-
-  playAudio() {
+  playAudio(){
     this.audioElement.load();
     this.audioElement.play();
   }
-
-  pauseAudio() {
+  pauseAudio(){
     this.audioElement.pause();
   }
-
-  handleClick(e){
-    switch(e.target.id){
+  selectTrackNumber(trackId){
+    this.setState({currentTrackIndex:trackId,playing:true},this.playAudio);
+  }
+  handleClick(e) {
+    switch (e.target.id) {
       case "play":
         this.setState((state, props) => {
           let currentTrackIndex = state.currentTrackIndex;
-          if (currentTrackIndex === 0){
+          if (currentTrackIndex === 0) {
             currentTrackIndex = 1;
-          }else{
-            return{
-              playing: true,
-              currentTrackIndex: currentTrackIndex
-            };
           }
-        }, this.playAudio);
+          return {
+            playing: true,
+            currentTrackIndex: currentTrackIndex
+          };
+        },this.playAudio);
         break;
-      
       case "pause":
-        this.setState({ playing: false}, this.pauseAudio);
+        this.setState({ playing: false },this.pauseAudio);
         break;
-
       case "prev":
         this.setState((state, props) => {
           let currentIndex = state.currentTrackIndex - 1;
-          if(currentIndex <= 0){
+          if (currentIndex <= 0) {
             return null;
-          }else{
-            return {
-              playing: true,
-              currentTrackIndex: currentIndex
-            };
+          } else {
+            return { playing:true,currentTrackIndex: currentIndex };
           }
-        }, this.playAudio);
+        },this.playAudio);
         break;
-
       case "next":
         this.setState((state, props) => {
           let currentIndex = state.currentTrackIndex + 1;
-          if(currentIndex > data.tracks.length){
+          if (currentIndex > data.tracks.length) {
             return null;
-          }else{
-            return{
-              playing: true,
-              currentTrackIndex: currentIndex
-            };
+          } else {
+            return { playing:true,currentTrackIndex: currentIndex };
           }
-        }, this.playAudio);
+        },this.playAudio);
         break;
-
       default:
         break;
     }
   }
-
   render() {
     return (
       <div className="App">
-        <div 
+        <div
           className="Artwork"
-          style={{ backgroundImage:  'url(' + data.artwork + ')'}}
+          style={{ backgroundImage: "url(" + data.artwork + ")" }}
         >
           <Controls onClick={this.handleClick} playing={this.state.playing} />
-          <audio 
-            ref={(audio) => {this.audioElement = audio}} 
-            src={"/songs/"+this.state.currentTrackIndex+".mp3"}
-          />
+          <audio ref={(audio)=>{this.audioElement = audio}} src={"/songs/"+this.state.currentTrackIndex+".mp3"}/>
         </div>
-
-        <TrackList 
+        <TrackList
           currentTrackIndex={this.state.currentTrackIndex}
           selectTrackNumber={this.selectTrackNumber}
         />
-
-        <div className="MusicCredit">
-          <a href="https://www.bensound.com">
-            https://www.bensound.com
-          </a>
-        </div>
+        <div className="MusicCredit">Music: <a href="https://www.bensound.com">https://www.bensound.com</a></div>
       </div>
     );
   }
--- react music player/src/Controls.js
+++ react music player/src/Controls.js
@@ -2,37 +2,32 @@ import React, { Component } from 'react';
 
 class Controls extends Component{
     render() {
-        return(
+        return (
             <div className="Controls">
-                <i 
-                    id="prev"
-                    className="fa fa-fw fa-fast-backward"
-                    onClick={this.props.onClick}
-                />  
-                
-                {!this.props.playing && 
-                    <i 
-                        id="play"
-                        onClick={this.props.onClick}
-                        className="fa fa-fw fa-play"
-                    />
-                }
-
-                {this.props.playing && 
-                    <i 
-                        id="pause"
-                        onClick={this.props.onClick}
-                        className="fa fa-fw fa-pause"
-                    />
-                }
-
-                <i 
-                    id="next"
-                    onClick={this.props.onClick}
-                    className="fa fa-fw fa-fast-forward"
-                />
+              <i
+                id="prev"
+                className="fa fa-fw fa-fast-backward"
+                onClick={this.props.onClick}
+              />
+              {!this.props.playing &&
+                <i
+                  id="play"
+                  onClick={this.props.onClick}
+                  className="fa fa-fw fa-play"
+                />}
+              {this.props.playing &&
+                <i
+                  id="pause"
+                  onClick={this.props.onClick}
+                  className="fa fa-fw fa-pause"
+                />}
+              <i
+                id="next"
+                className="fa fa-fw fa-fast-forward"
+                onClick={this.props.onClick}
+              />
             </div>
-        );
+          );
     }
 }
 
