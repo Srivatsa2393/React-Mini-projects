64f19e14587b61dcdd270a913f5ff395773bd6fe
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-22 22:32:13 +0530

    Added the data object for the app

---
+++ React social share/src/data.js
@@ -0,0 +1,18 @@
+var data = {
+    title: 'ReactJS Social Share',
+    author: 'Srivatsa',
+    image: 'https://www.mercedes-benz.com/wp-content/uploads/sites/3/2016/06/01_Mercedes-Benz-Vehicles-AMG-GT-R-1280x686-1280x686.jpg?format=750w',
+    url: 'https://github.com/Srivatsa2393',
+    share: [
+        {
+            type: 'facebook',
+            count: 43,
+            url: 'https://www.facebook.com/sharer/sharer.php?s=100&p[url]=https://github.com/Srivatsa2393'
+        },
+        {
+            type: 'twitter',
+            count: 7,
+            url: 'https://twitter.com/intent/tweet?hashtags=react,ui,dev&text=ReactJS%20Daily%20UI%20on%20Codepen&tw_p=tweetbutton&url=https://github.com/Srivatsa2393'
+        }
+    ]
+};
