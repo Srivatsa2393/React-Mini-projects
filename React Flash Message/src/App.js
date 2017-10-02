37d0cc06ad97cc4fe288b85beb2bf9bda2093b95
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-02 19:12:49 +0530

    basic scafolding of the StatusIcon component

--- React Flash Message/src/App.js
+++ React Flash Message/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import StatusIcon from './StatusIcon';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <StatusIcon />
         {this.props.message}
       </div>
     );
---
+++ React Flash Message/src/StatusIcon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StatusIcon extends Component{
+    render(){
+        return(
+            <div>
+                Status Icon component
+            </div>
+        );
+    }
+}
+
+export default StatusIcon;
