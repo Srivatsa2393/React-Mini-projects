0ed7f2a046c470dc857a5239ff54389d3bfb5196
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-02 19:18:25 +0530

    statusClass as class and status as props from the StatusIcon component

--- React Flash Message/src/App.js
+++ React Flash Message/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <StatusIcon />
+        <StatusIcon status="bad" />
         {this.props.message}
       </div>
     );
--- React Flash Message/src/StatusIcon.js
+++ React Flash Message/src/StatusIcon.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class StatusIcon extends Component{
     render(){
+        if(this.props.status === "good"){
+            var statusClass = 'fa fa-fw fa-check';
+        }else{
+            var statusClass = 'fa fa-fw fa-times';
+        }
         return(
-            <div>
-                Status Icon component
+            <div className="StatusIcon" data-status={this.props.status}>
+                <i className={statusClass}></i>
             </div>
         );
     }
