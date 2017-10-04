585fc94e320bb0b7ba4d72b84499e7259b3b6aaf
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-04 19:30:59 +0530

    Added the Information component

---
+++ React Add to cart page/src/Information.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Information extends Component{
+    render() {
+        return(
+            <div className="Information">
+                <div className="title">
+                    Lebron James Nike Air Zoom Pegasus 33 iD
+                </div>
+
+                <div className="Price">
+                    1550 SEK
+                </div>
+
+                <div className="Description">
+                    Mo Farah running shoes Nike Air Zoom Pegasus 33 iD combines perfect fit and fast 
+                    feel with responsive cushioning keeps you comfortable for long.
+                    <br /><br />
+                    This version honors Gen. historic six gold medals that he had taken two 
+                    at a time in three different championships in the world, with a hälmärke in gold metallic, 
+                    specialty printing and more.
+                </div>
+
+                <div className="Buy">
+                    <div className="label">
+                        +Add to Cart
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Information;
--- React Add to cart page/src/Product.js
+++ React Add to cart page/src/Product.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Information from './Information';
 
 class Product extends Component{
     render() {
         return(
             <div className="Product">
                 <Header />
+                <div className="Content">
+                    <Information />
+                </div>
             </div>
         );
     }
