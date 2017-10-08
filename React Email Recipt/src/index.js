d7a812e9f29e2ea36a431ddf1e5d3a13b90b9ac6
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-09 00:09:39 +0530

    Added the Styling for the Wrapper component

--- React Email Recipt/src/index.js
+++ React Email Recipt/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import Email from './Email';
 import registerServiceWorker from './registerServiceWorker';
 
