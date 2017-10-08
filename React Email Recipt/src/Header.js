f5bb8e7e0d3d13b58d78eaaec67c283635837972
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-08 23:53:32 +0530

    Added the new Header Component along with props as email component

--- React Email Recipt/src/Header.js
+++ React Email Recipt/src/Header.js
@@ -1,13 +1,14 @@
-import React from 'react';
-
-const Header = (props) => {
-    return(
-        <div className="Header">
-            <div className="wrapper">
-                {this.text}
-            </div>
+import React, { Component } from 'react';
+class Header extends Component{
+    render() {
+        return(
+            <div className="Header">
+                <div className="wrapper">
+                    <h1>{this.props.text}</h1>
+                </div>
         </div>
-    );
+        );
+    }
 }
 
 export default Header;
