8c04d53bf277ad8b50324b28eb9c179e6db27de4
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-10 19:47:45 +0530

    Comnpleted the Order Component as props

--- React Email Recipt/src/Content.js
+++ React Email Recipt/src/Content.js
@@ -8,7 +8,14 @@ class Content extends Component{
                 <div className="wrapper">
                     <h2>{this.props.title}</h2>
                     <p>{this.props.body}</p>
-                    <Order />
+                    <Order 
+                        title="Vatten" 
+                        size="A1 (841 x 594 mm)" 
+                        image="http://static1.squarespace.com/static/55acc005e4b098e615cd80e2/57c48747725e254fbeba117c/57c5304a9f745671dd496603/1472540751420/mock.jpg?format=1000w" 
+                        currency="SEK" price="799" 
+                        shipping="79"
+                        link="https://static.pexels.com/photos/34950/pexels-photo.jpg"
+                    />
                 </div>
             </div>
         );
--- React Email Recipt/src/Order.js
+++ React Email Recipt/src/Order.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 class Order extends Component{
     render() {
+        var total = parseInt(this.props.price) + parseInt(this.props.shipping);
         return(
             <div>
                 <h3>Order Details</h3>
                 <div className="Order"> 
                     <div className="OrderPreview">
-                        Order Component
+                        <a href={this.props.link}>
+                            <img src={this.props.image} alt={this.props.title}/>
+                        </a>
+                    </div>
+                    <div className="OrderDetails">
+                        <div className="Title">{this.props.title}</div>
+                        <div className="Size">{this.props.size}</div>
+                        <div className="Price">Price: {this.props.price} {this.props.currency}</div>
+                        <div className="Shipping">Shipping: {this.props.shipping} {this.props.currency}</div>
+                        <div className="Total"><strong>Total: {total} {this.props.currency}</strong></div>
                     </div>
                 </div>
             </div>
