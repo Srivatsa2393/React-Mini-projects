9906dde73e1f19d28b0b9ae2680f18192f1a653b
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-10 18:40:37 +0530

    Added the Content component

---
+++ React Email Recipt/src/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Content extends Component{
+    render() {
+        return(
+            <div className="Content">
+                <div className="wrapper">
+                 Content Component
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Content;
--- React Email Recipt/src/Email.js
+++ React Email Recipt/src/Email.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import Header from './Header';
 import HeroImage from './HeroImage';
+import Content from './Content';
 
 class Email extends Component {
   render() {
@@ -8,6 +9,7 @@ class Email extends Component {
       <div className="Email">
         <Header text="Your order" />
         <HeroImage url="http://static1.squarespace.com/static/55acc005e4b098e615cd80e2/5777cee5b8a79b6f0197ecba/57ae12a7e6f2e1c9bc4787d0/1471025851733/IMG_5246.jpg?format=2500w"/>
+        <Content />
       </div>
     );
   }
