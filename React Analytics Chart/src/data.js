3c4f71fa35f3578a7fc8a15a9e14b467a9087a59
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-12 03:19:19 +0530

    added the data for the graphs

---
+++ React Analytics Chart/src/data.js
@@ -0,0 +1,87 @@
+var chartData = [
+	{
+		id: 1,
+		name: 'Sign Up',
+		value: 5343
+	},
+	{
+		id: 2,
+		name: 'Checkout',
+		value: 1685
+	},
+	{
+		id: 3,
+		name: 'Landing Page',
+		value: 686
+	},
+	{
+		id: 4,
+		name: 'Calculator',
+		value: 2741
+	},
+	{
+		id: 5,
+		name: 'App Icon',
+		value: 190
+	},
+	{
+		id: 6,
+		name: 'User Profile',
+		value: 2566
+	},
+	{
+		id: 7,
+		name: 'Settings',
+		value: 2272
+	},
+	{
+		id: 8,
+		name: '404',
+		value: 187
+	},
+	{
+		id: 9,
+		name: 'Music Player',
+		value: 165
+	},
+	{
+		id: 10,
+		name: 'Social Share',
+		value: 1533
+	},
+	{
+		id: 11,
+		name: 'Flash Message',
+		value: 143
+	},
+	{
+		id: 12,
+		name: 'Single Product',
+		value: 1688
+	},
+	{
+		id: 13,
+		name: 'Direct Messaging',
+	 	value: 2113
+	},
+	{
+		id: 14,
+		name: 'Timer',
+		value: 3450
+	},
+	{
+		id: 15,
+		name: 'On/Off Switch',
+		value: 1407
+	},
+	{
+		id: 16,
+		name: 'Overlay / Popup',
+		value: 1236
+	},
+	{
+		id: 17,
+		name: 'Email Receipt',
+		value: 55
+	}
+];
