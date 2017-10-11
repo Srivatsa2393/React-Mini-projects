0fd86a8c73615b850429adda290bd09e6906379f
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-12 03:17:13 +0530

    React Analytics Chart boilerplate

---
+++ React Analytics Chart/src/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Chart.css';
+
+class App extends Component {
+render() {
+    return (
+      <div className="Chart">
+        Chart component
+      </div>
+    );
+  }
+}
+
+export default App;
