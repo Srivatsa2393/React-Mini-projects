8653c4931f269ac31a3afbec6bb072ce5513cfbf
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-12 17:41:45 +0530

    Added the ChartItem component

---
+++ React Analytics Chart/src/ChartItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChartItem extends Component{
+    render() {
+        return(
+            <div className="ChartItem">
+                ChartItem component
+            </div>
+        );
+    }
+}
+
+export default ChartItem;
--- React Analytics Chart/src/LineChart.js
+++ React Analytics Chart/src/LineChart.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import ChartItem from './ChartItem';
 
 class LineChart extends Component{
     render() {
-
+        // Load all data points
+		// Treat highest as 100%
+		// Scale others as percentages of the highest value
+        // Render
+        
         let highPoint = this.props.data.reduce((prev, cur) => {
             console.log(prev);
 
@@ -16,11 +21,13 @@ class LineChart extends Component{
 
         let chartItems = this.props.data.map((dataPoint, i) => {
             let percent = (dataPoint.value / highPoint.value) * 100 + '%';
+
+            return <ChartItem value={percent} name={dataPoint.name} amount={dataPoint.value} />
         });
 
         return(
             <div className="LineChart">
-                LineChart component
+                {chartItems}
             </div>
         );
     }
