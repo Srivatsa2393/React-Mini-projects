5d27d9699369c95f6c17e54c11a9c1ab4d49304b
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-13 03:51:48 +0530

    LeaderBoard styling using SASS

--- React LeaderBoard/src/LeaderBoard.js
+++ React LeaderBoard/src/LeaderBoard.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import data from './data';
 import './App.css';
 
 class LeaderBoard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: {data}
+    };
+  }
   render() {
     return (
       <div className="LeaderBoard">
