24e822f814a8cc6029cc093fb0b5893b63d92e5c
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-14 23:03:15 +0530

    Adding axios for ajax request and setting the state as soon as is the component is mounted in React

---
+++ React Location Tracker/src/Location.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class Location extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+      city: '',
+      country: '',
+      latLong: [-74.258188,40.7053111]
+    };
+  }
+
+  componentDidMount() {
+    var that = this;
+    axios.get("http://ip-api.com/json", function(data){
+      let city = data.city;
+      let country = data.country;
+      let lat = data.lat;
+      let lon = data.lon;
+
+      let latLong = [lat, lon];
+
+      this.setState({
+        city: city,
+        country: country,
+        latLong: latLong
+      });
+    });
+  }
+
+  render() {
+    let location = this.state.city + ',' + this.state.country;
+    return (
+      <div className="Location">
+       Location Component
+      </div>
+    );
+  }
+}
+
+export default Location;
