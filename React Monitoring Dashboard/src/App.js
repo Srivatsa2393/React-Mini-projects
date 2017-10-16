5fbfa9eccc98f511dcb5aebf57c95ba620a7e83c
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-16 19:29:50 +0530

    Adding the data in the constructor and lopping it over by using map function

--- React Monitoring Dashboard/src/App.js
+++ React Monitoring Dashboard/src/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [
+        {
+          name: 'Mentions Today',
+          value: 750
+        },
+        {
+          name: 'Social Mentions',
+          value: 243
+        },
+        {
+          name: 'Positive Mentions0',
+          value: 443
+        }
+      ]
+    };
+  }
   render() {
+
+    let numbers = this.state.data.map((number, i) => {
+      return <Group name={number.name} value={number.value} />
+    })
     return (
-      <div className="App">
+      <div className="container">
         App component
       </div>
     );
