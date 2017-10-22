48b239ed54ef20ac96c03ac7932c67404912747e
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-22 21:27:33 +0530

    setting the initial state

--- React Onboarding/src/App.js
+++ React Onboarding/src/App.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      step: 1
+    };
+  }
   render() {
     return (
       <div className="App">
-        App Component
+        <div className="Background" data-step={this.state.step}>
+
+        </div>
       </div>
     );
   }
