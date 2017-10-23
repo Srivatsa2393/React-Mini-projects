6e808caeaabde591819573330039ac683b25b2f9
Srivatsa S <srivatsashankaran@gmail.com>
2017-10-23 19:24:38 +0530

    Adding Slide to the Slides component

--- React Onboarding/src/Slides.js
+++ React Onboarding/src/Slides.js
@@ -5,7 +5,9 @@ class Slides extends Component{
         return(
             <div className="Slides" data-step={this.props.step}>
                 <div className="Wrapper">
-                    Slides Component
+                    <Slide text="The first step of onboarding, that is a thing." icon="comments-o" />
+                    <Slide text="Second step of onboarding, showing the things." icon="calendar-plus-o" />
+                    <Slide text="Final step of onboarding. Weeee, carousels!" icon="gears" />
                 </div>
             </div>
         );
