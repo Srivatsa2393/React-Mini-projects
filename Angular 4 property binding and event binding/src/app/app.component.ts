dff90a299bd406c4f2ab98c628df42fa73f10972
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-08 23:55:36 +0530

    Startgame working by adding the onIntervalFired() to the app-game-control

--- Angular 4 property binding and event binding/src/app/app.component.ts
+++ Angular 4 property binding and event binding/src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appComponentStartGame(event){
-    console.log(event.value);
-  }
-
-  appComponentStopGame(event){
-    console.log(event.value)
+  onIntervalFired(firedNumber: number){
+    console.log(firedNumber);
   }
 }
