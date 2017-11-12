8b95fd131c1e7e18e75b0bc5199a03a202f21606
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-12 23:04:29 +0530

    Creating a data service

--- Services and Dependency Injection in Angular/services-start/src/app/app.component.ts
+++ Services and Dependency Injection in Angular/services-start/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
+export class AppComponent implements OnInit {
+  /*   accounts = [
     {
       name: 'Master Account',
       status: 'active'
@@ -19,13 +22,21 @@ export class AppComponent {
       name: 'Hidden Account',
       status: 'unknown'
     }
-  ];
+  ]; */
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
+  /* onAccountAdded(newAccount: {name: string, status: string}) {
     this.accounts.push(newAccount);
   }
 
   onStatusChanged(updateInfo: {id: number, newStatus: string}) {
     this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  } */
+
+  accounts : {name: string, status: string}[] = [];
+
+  constructor( private accountsService: AccountsService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
   }
 }
